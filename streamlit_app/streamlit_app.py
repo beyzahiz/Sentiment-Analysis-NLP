1ca2b61313dc092387c36cc5c149b6b3d371886f
Beyza Hız <beyzahiz.03@gmail.com>
2026-03-24 16:28:22 +0300

    update

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -1,16 +1,27 @@
 import streamlit as st
 import requests
+import os
 
 st.title("Sentiment Analysis App")
+API_URL = os.getenv("https://sentiment-analysis-nlp-jt8o.onrender.com", "http://localhost:8000")
 
-text = st.text_area("Enter a movie review: ")
+text = st.text_area("Yorumunuzu girin:")
 
-if st.button("Analyze"):
-    response = requests.post(
-        "http://api:8000/predict", 
-        json={"text": user_input}
-    )
-
-    result = response.json()
-
-    st.write("Sentiment:", result["sentiment"])
+if st.button("Analiz Et"):
+    if text:
+        # Değişken ismini 'text' olarak düzelttik
+        response = requests.post(f"{API_URL}/predict", json={"text": text})
+        
+        if response.status_code == 200:
+            result = response.json()
+            sentiment = result["sentiment"]
+            
+            # Görselleştirme ekleyelim (CV'de şık durur)
+            if sentiment == "positive":
+                st.success(f"Sonuç: {sentiment} 😊")
+            else:
+                st.error(f"Sonuç: {sentiment} 😡")
+        else:
+            st.error("API hatası oluştu.")
+    else:
+        st.warning("Lütfen bir metin girin.")
